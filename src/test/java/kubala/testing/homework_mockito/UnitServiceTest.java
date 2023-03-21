c7bd6e0a3af0278373e7dcfaf4ca982a47d8e579
Jakub Kubala <j.kubala92@gmail.com>
2023-03-21 13:19:11 +0100

    write the rest of the tests: exceptions + finding unit by coordinates

--- src/test/java/kubala/testing/homework_mockito/UnitServiceTest.java
+++ src/test/java/kubala/testing/homework_mockito/UnitServiceTest.java
@@ -6,10 +6,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.sameInstance;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
@@ -48,5 +51,42 @@ class UnitServiceTest {
         assertThat(unit.getCargo().get(0), is(cargo));
     }
 
+    @Test
+    public void shouldThrowExceptionIfThereIsNoCargoToAdd() {
 
+        //given
+        Unit unit = new Unit(new Coordinates(0, 0), 10, 10);
+
+        given(cargoRepository.findCargoByName("package")).willReturn(Optional.empty());
+
+        //when
+        //then
+        assertThrows(NoSuchElementException.class, () -> unitService.addCargoByName(unit, "package"));
+    }
+
+    @Test
+    public void shouldReturnUnitByCoordinates() {
+
+        //given
+        Unit unit = new Unit(new Coordinates(0, 0), 10, 10);
+
+        given(unitRepository.getUnitByCoordinates(new Coordinates(0, 0))).willReturn(unit);
+
+        //when
+        Unit result = unitService.getUnitOn(new Coordinates(0,0));
+
+        //then
+        assertThat(result, sameInstance(unit));
+    }
+
+    @Test
+    public void shouldReturnExceptionWhenUnitNotFound() {
+
+        //given
+        given(unitRepository.getUnitByCoordinates(new Coordinates(0, 0))).willReturn(null);
+
+        //when
+        //then
+        assertThrows(NoSuchElementException.class, () -> unitService.getUnitOn(new Coordinates(0, 0)));
+    }
 }
